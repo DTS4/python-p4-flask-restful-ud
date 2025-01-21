63d1ab7ffcac85fb7c4d50c2019b6418fd4b58f4
Keith <keith.githinji@student.moringaschool.com>
2025-01-21 11:17:15 +0300

    Completed the lab

--- server/app.py
+++ server/app.py
@@ -44,6 +44,35 @@ class Newsletters(Resource):
             200,
         )
 
+    def patch(self, id):
+    
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+    def delete(self, id):
+    
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
         return response
 
     def post(self):
